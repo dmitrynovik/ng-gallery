1e3ef9f5a2cd54ccc5334997b5c5a8526666ed2a
dmitrynovik <cryinstone@gmail.com>
2017-07-01 12:20:08 +1000

    gallery component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: `./app.component.html`,
 })
 export class AppComponent  { 
+
+    //
+    // Default presets
+    //
+    selectedImage:Image = {src: "https://upload.wikimedia.org/wikipedia/commons/1/15/Operahuset_i_Sydney.JPG"};
+
+    images:Image[] = [
+      {src: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Sydney_Opera_House_-_Dec_2008.jpg"},
+      {src: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Sydney_Harbour_pano_at_night.jpg"},
+      {src: "https://static.pexels.com/photos/54610/sydney-opera-house-australia-54610.jpeg"},
+      {src: "https://upload.wikimedia.org/wikipedia/commons/7/72/Sydney_skyline_from_the_north_aerial_2010.jpg"}
+    ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { SlideComponent }  from './slide.component';
+import { GalleryComponent }  from './gallery.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, SlideComponent ],
+  declarations: [ AppComponent, SlideComponent, GalleryComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
