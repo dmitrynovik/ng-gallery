591cc01a711deeba7eab629b77c3576528083aeb
dmitrynovik <cryinstone@gmail.com>
2017-07-01 11:27:48 +1000

    added slide component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { SlideComponent }  from './slide.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, SlideComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
